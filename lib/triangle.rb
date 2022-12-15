f11ca08704dbae0b298f8702576c4a7ecf99b7f5
David Tewkesbury <tewkesburyd@hotmail.com>
2022-12-14 22:13:57 -0700

    commit tree ea7b893ffc4d3ee1d416dad788c5262c2c143199

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize side1, side2, side3
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    validate
+    if @side1 == @side2 && @side3 == @side2
+      :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_side
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    @side1 + @side2 > @side3 && @side1 + @side3 > @side2 && @side2 + @side3 > @side1
+  end
+
+  def validate
+    raise TriangleError unless validate_side && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError 
+  end
+
 end
